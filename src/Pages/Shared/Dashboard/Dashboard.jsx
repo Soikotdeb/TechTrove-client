36f9ff67a21362c0d2c620be02c22931f192c23c
soikotdeb <sdsoikot424@gmail.com>
2023-08-08 17:58:28 +0600

    some right and dynamic date time added

--- src/Pages/Shared/Dashboard/Dashboard.jsx
+++ src/Pages/Shared/Dashboard/Dashboard.jsx
@@ -8,12 +8,34 @@ import { FaChalkboardTeacher } from "react-icons/fa";
 import { FiShoppingCart } from "react-icons/fi";
 import logo from "../../../assets/image/company logo.png";
 import UseAdmin from "../../../Hook/UseAdmin";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../../Provider/AuthProvider";
 
 const Dashboard = () => {
   const [isAdminOrInstructor, isAdminOrInstructorLoading] = UseAdmin();
   const { loading } = useContext(AuthContext);
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const dayOfWeek = daysOfWeek[currentTime.getDay()];
 
   if (loading || isAdminOrInstructorLoading) {
     return (
@@ -50,38 +72,71 @@ const Dashboard = () => {
           Open drawer
         </label>
       </div>
-
+      {/* Admin Dashboard */}
       {isAdminOrInstructor === "admin" && (
         <div className="drawer-side">
           <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-          <ul className="menu p-4 w-80 h-full bg-gray-300 text-base-content font-bold text-1xl">
-            <p className="text-orange-600 font-bold text-3xl mb-4">
-              Welcome to Admin Dashboard
+          <ul className="menu p-4 w-80 h-full text-white bg-gray-900  font-bold text-1xl">
+            <p className="text-orange-600 font-bold text-2xl mb-4">
+              <div className="flex items-center">
+                <img
+                  className="w-9 h-12 rounded-2xl"
+                  title="TechTrove GadgetBazar"
+                  src={logo}
+                  alt=""
+                />{" "}
+                <p className="text-red-500 text-3xl ml-2 hover:text-red-600 flex">
+                  Tech <p className="text-red-600 hover:text-red-700">Trove</p>{" "}
+                </p>
+              </div>
             </p>
+
             <li>
-              <NavLink to="/">
+              <NavLink className="hover:text-red-400" to="/">
                 {" "}
                 <AiOutlineHome size={24} />
                 Back To Admin Home
               </NavLink>
             </li>
             <li>
-              <NavLink to="/dashboard/ManageUsers">
+              <NavLink
+                className="hover:text-red-400"
+                to="/dashboard/ManageUsers"
+              >
                 {" "}
                 <BsPeopleFill size={24} />
                 Manage Users
               </NavLink>
             </li>
             <li>
-              <NavLink to="/dashboard/ManageClasses">
+              <NavLink
+                className="hover:text-red-400"
+                to="/dashboard/ManageClasses"
+              >
                 <BiCog size={24} />
                 Manage Classes
               </NavLink>
             </li>
+
+            <div className="absolute bottom-10 left-0 right-0">
+              <p className="text-center text-gray-400 mt-2">
+                {currentTime.toLocaleString()} ({dayOfWeek})
+              </p>
+              <p className="text-center text-gray-400">
+                <span className="text-orange-500 hover:text-orange-600">
+                  tech
+                </span>
+                <span className="text-orange-700 hover:text-orange-800">
+                  Trove
+                </span>{" "}
+                All rights reserved
+              </p>
+            </div>
+
           </ul>
         </div>
       )}
-
+      {/* instructor Dashboard */}
       {isAdminOrInstructor === "instructor" && (
         <div className="drawer-side">
           <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
@@ -98,29 +153,49 @@ const Dashboard = () => {
               </p>
             </div>
             <li>
-              <NavLink to="/">
+              <NavLink className="hover:text-red-400" to="/">
                 {" "}
                 <AiOutlineHome size={24} />
                 Back To Instructor Home
               </NavLink>
             </li>
             <li>
-              <NavLink to="/dashboard/AddClass">
+              <NavLink className="hover:text-red-400" to="/dashboard/AddClass">
                 {" "}
                 <BiPlus size={24} />
                 Add a Class
               </NavLink>
             </li>
             <li>
-              <NavLink to="/dashboard/MyClasses" activeClassName="active">
+              <NavLink
+                className="hover:text-red-400"
+                to="/dashboard/MyClasses"
+                activeClassName="active"
+              >
                 <IoMdSchool size={24} />
                 My Classes
               </NavLink>
             </li>
+
+            <div className="absolute bottom-10 left-0 right-0">
+              <p className="text-center text-gray-400 mt-2">
+                {currentTime.toLocaleString()} ({dayOfWeek})
+              </p>
+              <p className="text-center text-gray-400">
+                <span className="text-orange-500 hover:text-orange-600">
+                  tech
+                </span>
+                <span className="text-orange-700 hover:text-orange-800">
+                  Trove
+                </span>{" "}
+                All rights reserved
+              </p>
+            </div>
+
           </ul>
         </div>
       )}
-
+      {/* user Dashboard */}
       {isAdminOrInstructor === "user" && (
         <div className="drawer-side text-white">
           <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
@@ -184,6 +259,22 @@ const Dashboard = () => {
                 My Enrolled Classes
               </NavLink>
             </li>
+
+            <div className="absolute bottom-10 left-0 right-0">
+              <p className="text-center text-gray-400 mt-2">
+                {currentTime.toLocaleString()} ({dayOfWeek})
+              </p>
+              <p className="text-center text-gray-400">
+                <span className="text-orange-500 hover:text-orange-600">
+                  tech
+                </span>
+                <span className="text-orange-700 hover:text-orange-800">
+                  Trove
+                </span>{" "}
+                All rights reserved
+              </p>
+            </div>
+            
           </ul>
         </div>
       )}
