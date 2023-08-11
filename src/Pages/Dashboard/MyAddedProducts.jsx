2356a782081bcd6fe67460b4225057d5d2749d8e
soikotdeb <sdsoikot424@gmail.com>
2023-08-11 12:25:38 +0600

    my addedproduct section some style added

--- src/Pages/Dashboard/MyAddedProducts.jsx
+++ src/Pages/Dashboard/MyAddedProducts.jsx
@@ -2,6 +2,7 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from './../../Provider/AuthProvider';
 import { Link } from "react-router-dom";
+import { FaEdit, FaTrash } from 'react-icons/fa';
 
 const MyAddedProducts = () => {
     const { user } = useContext(AuthContext);
@@ -49,17 +50,17 @@ const MyAddedProducts = () => {
                         />
                            <p className="text-lg font-semibold">productName: {product.productName}</p>
                         <div className="text-gray-600 text-base" >
-                        <p>Color: {product.productColor}</p>
-                        <p>Storage: {product.storage}</p>
-                        <p>Description: {product.description}</p>
-                        <p>Category: {product.category}</p>
-                        <p>MadeIn: {product.madeIn}</p>
-                        <p>DiscountAmount: {product.discountAmount}</p>
-                        <p>Quantity: {product.productQuantity}</p>
+                        <p><span className="font-bold text-gray-700 hover:underline">Color: </span> {product.productColor}</p>
+                        <p><span className="font-bold text-gray-700 hover:underline">Storage: </span> {product.storage}</p>
+                        <p><span className="font-bold text-gray-700 hover:underline">Category: </span> {product.category}</p>
+                        <p><span className="font-bold text-gray-700 hover:underline">MadeIn</span> : {product.madeIn}</p>
+                        <p><span className="font-bold text-gray-700 hover:underline">DiscountAmount</span> : {product.discountAmount}</p>
+                        <p><span className="font-bold text-gray-700 hover:underline">Quantity: </span> {product.productQuantity}</p>
+                        <p><span className="font-bold text-gray-700 hover:underline">Description: </span> {product.description}</p>
                         </div>
                         <div className="flex justify-between mt-2">
-                        <Link className="bg-green-500 flex items-center font-semibold hover:text-purple-300 hover:bg-green-600 text-white px-6 py-3 rounded-full focus:outline-none shadow-md transition-shadow duration-300 mb-2">Update</Link>
-                        <Link className="bg-red-500 flex items-center font-semibold hover:text-purple-300 hover:bg-red-600 text-white px-6 py-3 rounded-full focus:outline-none shadow-md transition-shadow duration-300 mb-2">Delete</Link>
+                        <Link className="bg-green-500 flex items-center font-semibold hover:text-purple-300 hover:bg-green-600 text-white px-6 py-3 rounded-full focus:outline-none shadow-md transition-shadow duration-300 mb-2"> <FaEdit title="Click To Edit" /></Link>
+                        <Link className="bg-red-500 flex items-center font-semibold hover:text-purple-300 hover:bg-red-600 text-white px-6 py-3 rounded-full focus:outline-none shadow-md transition-shadow duration-300 mb-2"> <FaTrash title="Click To Delete" /></Link>
                         </div>
                     </div>
                 ))}
@@ -69,81 +70,3 @@ const MyAddedProducts = () => {
 };
 
 export default MyAddedProducts;
-
-// import React, { useContext, useEffect, useState } from "react";
-// import { AuthContext } from './../../Provider/AuthProvider';
-// import { Link } from "react-router-dom";
-// import AOS from "aos";
-// import "aos/dist/aos.css";
-
-// const MyAddedProducts = () => {
-//     const { user } = useContext(AuthContext);
-//     const [products, setProducts] = useState([]);
-
-//     useEffect(() => {
-//         const fetchUserData = async () => {
-//             try {
-//                 const response = await fetch(`http://localhost:5000/users/instructor/myAddedProducts/${encodeURIComponent(user.email)}`);
-//                 if (response.ok) {
-//                     const data = await response.json();
-//                     setProducts(data);
-//                 } else {
-//                     console.log("Error:", response.status);
-//                 }
-//             } catch (error) {
-//                 console.log("Error:", error);
-//             }
-//         };
-//         fetchUserData();
-//     }, [user.email]);
-
-//     const [hoveredIndex, setHoveredIndex] = useState(null);
-
-//     // Initialize AOS
-//     useEffect(() => {
-//         AOS.init();
-//     }, []);
-
-//     return (
-//         <div className="p-4 bg-gray-800 w-full h-full">
-//             <div className="border border-green-400 text-white mb-2">
-//                 <marquee behavior="alternate" direction="left">
-//                     <p className="text-purple-300"> {user.displayName} - Your All Added Products Here</p>
-//                 </marquee>
-//             </div>
-//             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-//                 {products.map((product, index) => (
-//                     <div
-//                         key={product._id}
-//                         className="bg-white shadow-md rounded-lg p-4 transition transform hover:-translate-y-2 hover:shadow-xl"
-//                         data-aos="fade-up" // Add AOS animation attribute
-//                     >
-//                         <img
-//                             onMouseEnter={() => setHoveredIndex(index)}
-//                             onMouseLeave={() => setHoveredIndex(null)}
-//                             src={hoveredIndex === index ? product.productImages[2] : product.productImages[1]}
-//                             alt={product.productName}
-//                             className="w-full h-48 object-cover mb-2 rounded-lg"
-//                         />
-//                         <p className="text-lg font-semibold">productName: {product.productName}</p>
-//                         <div className="text-gray-600 text-base" >
-//                         <p>Color: {product.productColor}</p>
-//                         <p>Storage: {product.storage}</p>
-//                         <p>Description: {product.description}</p>
-//                         <p>Category: {product.category}</p>
-//                         <p>MadeIn: {product.madeIn}</p>
-//                         <p>DiscountAmount: {product.discountAmount}</p>
-//                         <p>Quantity: {product.productQuantity}</p>
-//                         </div>
-//                         <div className="flex justify-between mt-2">
-//                             <Link className="bg-green-500 flex items-center font-semibold hover:text-purple-300 hover:bg-green-600 text-white px-6 py-3 rounded-full focus:outline-none shadow-md transition-shadow duration-300 mb-2">Update</Link>
-//                             <Link className="bg-red-500 flex items-center font-semibold hover:text-purple-300 hover:bg-red-600 text-white px-6 py-3 rounded-full focus:outline-none shadow-md transition-shadow duration-300 mb-2">Delete</Link>
-//                         </div>
-//                     </div>
-//                 ))}
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default MyAddedProducts;
