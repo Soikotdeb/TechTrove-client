0937ca22247a1a4693c1dd0e052d40a8b3968cb9
soikotdeb <sdsoikot424@gmail.com>
2023-08-21 10:58:35 +0600

    offer  delete succesfully

--- src/Pages/NewOffer/NewOffer.jsx
+++ src/Pages/NewOffer/NewOffer.jsx
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { motion } from "framer-motion";
-import { FaArrowAltCircleRight, FaTags } from "react-icons/fa";
+import { FaArrowAltCircleRight, FaTags, FaTrashAlt } from "react-icons/fa";
 import discount from "../../assets/image/discount.png";
 import specialOffer from "../../assets/image/specialOffer.png";
 import limitedTime from "../../assets/image/limitedTime.png";
 import { Link } from "react-router-dom";
+import UseAdmin from "../../Hook/UseAdmin";
+import Swal from "sweetalert2";
 
 const LatestOffers = () => {
   const [latestOffers, setLatestOffers] = useState([]);
+  const [isAdminOrInstructor] = UseAdmin();
 
-  const { data: fetchedOffers = [] } = useQuery(["latestOffers"], async () => {
+  const { data: fetchedOffers = [],refetch } = useQuery(["latestOffers"], async () => {
     const res = await fetch("http://localhost:5000/LatestOffers");
     return res.json();
   });
@@ -23,6 +26,37 @@ const LatestOffers = () => {
     // Store specific offer information in local storage
     localStorage.setItem("OfferDetails", JSON.stringify(offer));
   };
+
+  const handleDelete = async (id) => {
+    try {
+      const result = await Swal.fire({
+          title: "Are you sure you want to delete?",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonText: "Yes",
+          cancelButtonText: "No",
+      });
+
+      if (result.isConfirmed) {
+          await fetch(`http://localhost:5000/LatestOffer/${id}`, {
+              method: "DELETE",
+          });
+          await Swal.fire({
+              icon: "success",
+              title: "Offer Deleted",
+              text: "Offer has been deleted successfully.",
+              confirmButtonText: "OK",
+          });
+          // Refetch Question after delete
+          refetch();
+      }
+  } catch (error) {
+      console.log(error);
+  }
+
+  };
+
+
 
   return (
     <div className="container mx-auto py-8">
@@ -52,6 +86,21 @@ const LatestOffers = () => {
                 <FaTags className="text-gray-100 mr-1" />
                 -${offer.discountAmount}
               </p>
+              <div>
+                {isAdminOrInstructor === "admin" ||
+                isAdminOrInstructor === "instructor" ? (
+                  <Link
+                  onClick={() => handleDelete(offer._id)}
+                    className="absolute top-16 right-3 p-2 rounded-lg flex items-center"
+                    title="Delete Offer"
+                  >
+                    <FaTrashAlt
+                      size={24}
+                      className="text-red-600 hover:text-red-700  mr-1"
+                    />
+                  </Link>
+                ) : null}
+              </div>
               <img
                 className="absolute top-14 left-3 w-10 rounded-lg"
                 src={discount}
@@ -70,15 +119,16 @@ const LatestOffers = () => {
             </div>
             <div className="p-4">
               <h3 className="text-lg font-semibold mb-2">
-              ➺ {offer.productName}
+                ➺ {offer.productName}
               </h3>
               <div className="flex items-center justify-between mt-3">
                 <p className="text-gray-600">
                   <span className="font-semibold">✪ Regular Price :</span>{" "}
                   {offer.price}
                 </p>
-                <Link to={`/fullDetails/${offer._id}`}
-                 onClick={() => handleFullDetails(offer)}
+                <Link
+                  to={`/fullDetails/${offer._id}`}
+                  onClick={() => handleFullDetails(offer)}
                 >
                   <FaArrowAltCircleRight
                     className="text-gray-600"
