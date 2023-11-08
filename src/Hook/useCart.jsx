e060e672f7879874077f36c9b4a25ef0315db4ee
soikotdeb <sdsoikot424@gmail.com>
2023-11-08 12:50:58 +0600

    shopping cart data get and show cart added total product

---
+++ src/Hook/useCart.jsx
@@ -0,0 +1,31 @@
+import { useContext } from "react";
+import { AuthContext } from "../Provider/AuthProvider";
+import useAxiosSecure from "./useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
+
+
+const useCart = () => {
+const {user,loading}= useContext(AuthContext)
+
+// const token = localStorage.getItem('access-token')
+const [axiosSecure] = useAxiosSecure();
+
+const { refetch, data: cart = [] } = useQuery({
+    queryKey: ['carts', user?.email],
+    enabled:!loading,
+    // queryFn: async () => {
+    //     const res = await fetch(`https://bistro-boss-server-orcin.vercel.app/carts?email=${user?.email}`,{headers:{
+    //         authorization: `bearer ${token}`
+    //     }})
+    //     return res.json();
+    // },
+    queryFn: async () => {
+        const res = await axiosSecure(`/carts?email=${user?.email}`)
+        return res.data;
+    },
+})
+
+return [cart, refetch]
+
+}
+export default useCart;
--- src/Pages/Shared/Navbar/MainNav.jsx
+++ src/Pages/Shared/Navbar/MainNav.jsx
@@ -1,5 +1,12 @@
-
-import { FaExchangeAlt, FaFileAlt, FaHeart, FaSearch, FaShare, FaShoppingCart, FaUserCircle } from "react-icons/fa";
+import {
+  FaExchangeAlt,
+  FaFileAlt,
+  FaHeart,
+  FaSearch,
+  FaShare,
+  FaShoppingCart,
+  FaUserCircle,
+} from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { HiOutlineLogin } from "react-icons/hi";
 import { HiOutlineLogout } from "react-icons/hi";
@@ -9,20 +16,18 @@ import { AuthContext } from "../../../Provider/AuthProvider";
 import { RiDashboardFill } from "react-icons/ri"; // Remix Icon icons
 import { FiSettings, FiUserPlus } from "react-icons/fi";
 import { AiOutlineCloseCircle, AiOutlineUser } from "react-icons/ai";
-import { MdLocalAtm,MdNotificationsActive,MdLiveHelp } from 'react-icons/md';
-
-
-
+import { MdLocalAtm, MdNotificationsActive, MdLiveHelp } from "react-icons/md";
+import useCart from "../../../Hook/useCart";
 
 const MainNav = () => {
   const { user, logOut } = useContext(AuthContext);
-const [isModalOpen, setIsModalOpen] = useState(false); // State to manage the modal visibility
-
+  const [isModalOpen, setIsModalOpen] = useState(false); // State to manage the modal visibility
+  const [cart] = useCart();
 
   const handleLogout = () => {
     logOut()
       .then(() => {
-        localStorage.removeItem('TechTrove-Access-token')
+        localStorage.removeItem("TechTrove-Access-token");
       })
       .catch((error) => {
         console.log(error);
@@ -64,8 +69,13 @@ const [isModalOpen, setIsModalOpen] = useState(false); // State to manage the mo
                 </a>
               </li>
               <li>
-                <a>
+                <a className="relative">
                   <FaShoppingCart className="w-6 h-6" />
+                  {cart.length > 0 && (
+                <div className="absolute top-2 left-4 transform translate-x-2 -translate-y-2 bg-red-500 text-white rounded-xl w-5 h-3 flex items-center justify-center ">
+                  {cart.length}
+                </div>
+              )}
                 </a>
               </li>
               <li>
@@ -77,116 +87,187 @@ const [isModalOpen, setIsModalOpen] = useState(false); // State to manage the mo
           </div>
           <img className="w-9 h-12 rounded" src={logo} alt="" />
           <a className="btn btn-ghost hidden md:block normal-case text-4xl hover:text-purple-600">
-            <p className="text-gray-500 hover:text-red-600 flex">Tech <p className="text-gray-600 hover:text-red-700">Trove</p> </p>
+            <p className="text-gray-500 hover:text-red-600 flex">
+              Tech <p className="text-gray-600 hover:text-red-700">Trove</p>{" "}
+            </p>
           </a>
         </div>
         <div className="navbar-center hidden lg:flex">
+          <div className="relative">
+            <input
+              type="text"
+              placeholder="Search..."
+              className="py-2 pr-10 pl-4 rounded-full border border-gray-300 focus:outline-none focus:ring focus:border-blue-300"
+            />
+            <Link className="absolute top-1/2 right-4 transform -translate-y-1/2">
+              <FaSearch className="text-gray-900" />
+            </Link>
+          </div>
 
-<div className="relative">
-      <input
-        type="text"
-        placeholder="Search..."
-        className="py-2 pr-10 pl-4 rounded-full border border-gray-300 focus:outline-none focus:ring focus:border-blue-300"
-
-      />
-      <Link 
-      className="absolute top-1/2 right-4 transform -translate-y-1/2">
-        <FaSearch className="text-gray-900" />
-      </Link>
-    </div>
-
- 
-            <ul className="menu menu-horizontal flex items-center gap-4">
+          <ul className="menu menu-horizontal flex items-center gap-4">
             <li>
               <Link>
                 <FaHeart className="w-6 h-6 text-gray-500 hover:text-purple-700 transition-colors duration-300" />
               </Link>
             </li>
-            <li>
-              <Link>
-                <FaShoppingCart className="w-6 h-6 text-gray-500 hover:text-purple-700 transition-colors duration-300" />
+            <li className="relative">
+              <Link
+                to="/cart"
+                className="text-gray-500 hover:text-purple-700 transition-colors duration-300"
+              >
+                <FaShoppingCart className="w-6 h-6" />
               </Link>
+              {cart.length > 0 && (
+                <div className="absolute top-0 right-1 transform translate-x-2 -translate-y-2 bg-red-500 text-white rounded-xl w-2 h-2 flex items-center justify-center ">
+                  {cart.length}
+                </div>
+              )}
             </li>
+
             <li>
               <Link>
                 <FaExchangeAlt className="w-6 h-6 text-gray-500 hover:text-purple-700 transition-colors duration-300" />
               </Link>
             </li>
           </ul>
-
         </div>
 
         <div>
-         <Link to="" title="Notifications"> <MdNotificationsActive className="w-6 h-6 me-4 lg:ms-5 text-gray-500 hover:text-purple-700 transition-colors duration-300" /> </Link> 
+          <Link to="" title="Notifications">
+            {" "}
+            <MdNotificationsActive className="w-6 h-6 me-4 lg:ms-5 text-gray-500 hover:text-purple-700 transition-colors duration-300" />{" "}
+          </Link>
         </div>
 
-
-        <Link onClick={openModal}  className="lg:ml-3 ">
+        <Link onClick={openModal} className="lg:ml-3 ">
           {user && (
             <div
               title={user.displayName || "Not Available User Name"}
               className="avatar online"
             >
               <div className="w-9 rounded-full border border-red-800">
-                <img src={user?.photoURL} alt="User Profile" /> 
+                <img src={user?.photoURL} alt="User Profile" />
               </div>
             </div>
           )}
         </Link>
 
         {isModalOpen && (
-        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 z-50">
-          <div className="text-white rounded-lg p-8 transition duration-300 ease-in-out bg-gradient-to-r from-blue-500 to-purple-500">
-            <ul className="grid gap-4">
-              <div>           
-              <li>
-                <p className="text-center">--------- All Account ---------</p>
-                <p className="flex items-center gap-1"><FaUserCircle/> {user.displayName } <Link className= " ms-4 hover:text-green-400 border p-y-2 text-green-300"><small>Switch</small></Link> </p>
-              </li>
-              <hr className="border-t-4 border-white mt-2" />
-              </div>
-              <li>
-                <Link to="dashboard/welcome"> <p className="flex items-center gap-1 hover:text-purple-200"><RiDashboardFill className="w-6 h-6" />Dashboard</p> </Link>
-              </li>
-              <li>
-                <Link to="/add-account"> <p className="flex items-center gap-1 hover:text-purple-200"><FiUserPlus className="w-6 h-6" />Add More Account</p> </Link>
-              </li>
-              <li>
-                <Link to="/profile"> <p className="flex items-center gap-1 hover:text-purple-200"> <AiOutlineUser className="w-4 h-4" />Update Profile</p> </Link>
-              </li>
-              <li>
-                <Link to="/profile"> <p className="flex items-center gap-1 hover:text-purple-200"> <MdLiveHelp className="w-4 h-4" />Live Support</p> </Link>
-              </li>
-              <li>
-                <Link title="We offer a 30-day money-back guarantee" to="/"> <p className="flex items-center gap-1 hover:text-purple-200"> <MdLocalAtm  className="w-4 h-4" />Refund Policy</p> </Link>
-              </li>
-              <li>
-                <Link to="/settings"> <p className="flex items-center gap-1 hover:text-purple-200"><FiSettings className="w-4 h-4 " /> Settings</p></Link>
-              </li>
-              <li>
-                <Link to="/invite"> <p className="flex items-center gap-1 hover:text-purple-200"><FaShare></FaShare>Invite Friend </p></Link>
-              </li>
+          <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 z-50">
+            <div className="text-white rounded-lg p-8 transition duration-300 ease-in-out bg-gradient-to-r from-blue-500 to-purple-500">
+              <ul className="grid gap-4">
+                <div>
+                  <li>
+                    <p className="text-center">
+                      --------- All Account ---------
+                    </p>
+                    <p className="flex items-center gap-1">
+                      <FaUserCircle /> {user.displayName}{" "}
+                      <Link className=" ms-4 hover:text-green-400 border p-y-2 text-green-300">
+                        <small>Switch</small>
+                      </Link>{" "}
+                    </p>
+                  </li>
+                  <hr className="border-t-4 border-white mt-2" />
+                </div>
+                <li>
+                  <Link to="dashboard/welcome">
+                    {" "}
+                    <p className="flex items-center gap-1 hover:text-purple-200">
+                      <RiDashboardFill className="w-6 h-6" />
+                      Dashboard
+                    </p>{" "}
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/add-account">
+                    {" "}
+                    <p className="flex items-center gap-1 hover:text-purple-200">
+                      <FiUserPlus className="w-6 h-6" />
+                      Add More Account
+                    </p>{" "}
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/profile">
+                    {" "}
+                    <p className="flex items-center gap-1 hover:text-purple-200">
+                      {" "}
+                      <AiOutlineUser className="w-4 h-4" />
+                      Update Profile
+                    </p>{" "}
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/profile">
+                    {" "}
+                    <p className="flex items-center gap-1 hover:text-purple-200">
+                      {" "}
+                      <MdLiveHelp className="w-4 h-4" />
+                      Live Support
+                    </p>{" "}
+                  </Link>
+                </li>
+                <li>
+                  <Link title="We offer a 30-day money-back guarantee" to="/">
+                    {" "}
+                    <p className="flex items-center gap-1 hover:text-purple-200">
+                      {" "}
+                      <MdLocalAtm className="w-4 h-4" />
+                      Refund Policy
+                    </p>{" "}
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/settings">
+                    {" "}
+                    <p className="flex items-center gap-1 hover:text-purple-200">
+                      <FiSettings className="w-4 h-4 " /> Settings
+                    </p>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/invite">
+                    {" "}
+                    <p className="flex items-center gap-1 hover:text-purple-200">
+                      <FaShare></FaShare>Invite Friend{" "}
+                    </p>
+                  </Link>
+                </li>
 
-              <li>
-                <Link to="/terms"> <p className="flex items-center gap-1 hover:text-purple-200"> <FaFileAlt className="w-4 h-4" /> terms and conditions</p></Link>
-              </li>
-
-            </ul>
-            <button onClick={closeModal} className="btn bg-red-100 mt-4 w-full hover:bg-red-300">
-            <AiOutlineCloseCircle className="w-6 h-6 text-red-400 hover:text-red-800" />
-            </button>
+                <li>
+                  <Link to="/terms">
+                    {" "}
+                    <p className="flex items-center gap-1 hover:text-purple-200">
+                      {" "}
+                      <FaFileAlt className="w-4 h-4" /> terms and conditions
+                    </p>
+                  </Link>
+                </li>
+              </ul>
+              <button
+                onClick={closeModal}
+                className="btn bg-red-100 mt-4 w-full hover:bg-red-300"
+              >
+                <AiOutlineCloseCircle className="w-6 h-6 text-red-400 hover:text-red-800" />
+              </button>
+            </div>
           </div>
-        </div>
-      )}
-        
+        )}
 
         <div className="navbar-end ms-10">
           {user ? (
-            <Link onClick={handleLogout} className="btn hover:text-purple-500 text-gray-500 font-extrabold">
+            <Link
+              onClick={handleLogout}
+              className="btn hover:text-purple-500 text-gray-500 font-extrabold"
+            >
               LogOut <HiOutlineLogout className="ml-1 w-5 h-5" />
             </Link>
           ) : (
-            <Link to="/logIn" className="btn hover:text-purple-500 text-gray-500 font-extrabold">
+            <Link
+              to="/logIn"
+              className="btn hover:text-purple-500 text-gray-500 font-extrabold"
+            >
               LogIn <HiOutlineLogin className="ml-1 w-5 h-5" />
             </Link>
           )}
@@ -197,4 +278,3 @@ const [isModalOpen, setIsModalOpen] = useState(false); // State to manage the mo
 };
 
 export default MainNav;
-
