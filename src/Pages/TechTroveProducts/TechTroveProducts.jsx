0a76115d7520a2e77c0248ba2f76c71d7ad44ed7
soikotdeb <sdsoikot424@gmail.com>
2023-11-10 21:37:54 +0600

    tech trove product section data loaded

--- src/Pages/TechTroveProducts/TechTroveProducts.jsx
+++ src/Pages/TechTroveProducts/TechTroveProducts.jsx
@@ -1,7 +1,9 @@
+
 import React, { useEffect, useState } from "react";
 import { Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 import AOS from "aos";
+import Swal from "sweetalert2";
 import { Link } from "react-router-dom";
 
 const TechTroveProducts = () => {
@@ -12,14 +14,14 @@ const TechTroveProducts = () => {
 
     const fetchData = async () => {
       try {
-        const response = await fetch('http://localhost:5000/TechTrove');
+        const response = await fetch("http://localhost:5000/TechTrove");
         if (!response.ok) {
-          throw new Error('Failed to fetch TechTrove data');
+          throw new Error("Failed to fetch TechTrove data");
         }
         const data = await response.json();
         setTechTroveProduct(data);
       } catch (error) {
-        console.error('Error:', error);
+        console.error("Error:", error);
       }
     };
 
@@ -31,6 +33,36 @@ const TechTroveProducts = () => {
 
     return () => clearInterval(intervalId);
   }, []);
+
+  const openModal = (product) => {
+    Swal.fire({
+      title: `${product.productName}`,
+      html: `
+        <div class="w-full h-48 mb-2">
+         <hr/>
+          <img src=${product.productImages[0]} class="w-1/2 h-full object-cover mx-auto" />
+        </div>
+        <hr/>
+        <div class="flex flex-wrap gap-2 justify-center">
+          <div class="flex flex-col items-center hover:bg-gray-100 p-2 rounded-lg">
+            <p class="text-teal-800 mb-2">Price: ৳ ${product.price} /BD</p>
+            <p class="text-blue-800">Color: ${product.productColor}</p>
+          </div>
+          <div class="flex flex-col items-center hover:bg-gray-100  rounded-lg">
+            <p class="text-purple-800 mb-1">Storage: ${product.storage}</p>
+            <p class="text-red-800 mb-1">Made In By: ${product.madeIn}</p>
+          </div>
+            <p class="text-green-800"> ${product.productQuantity} Product Available </p>
+            <p class="text-indigo-800">Discount: ${product.discountAmount} / Tk</p>
+            <p class="text-yellow-800">${product.description}</p>
+        </div>
+      `,
+    });
+  };
+  
+  
+  
+  
 
   return (
     <div className="bg-gray-100 rounded-lg p-4">
@@ -47,28 +79,27 @@ const TechTroveProducts = () => {
             <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-2 p-2">
               {TechTrove.map((product) => (
                 <div key={product._id} className="max-w-md mx-auto mb-8">
-                  
                   <div className="bg-gray-400 antialiased text-gray-900">
                     <img
                       src={product.productImages[0]}
                       alt="random image"
                       className="w-full h-96 object-cover object-center rounded-lg shadow-md"
                     />
-                    <div className="relative px-3 -mt-16 py-1">
+                    <div className="relative px-3 -mt-20 py-1">
                       <div className="bg-white p-8 rounded-lg shadow-lg">
                         <div className="flex items-baseline">
                           <span className="bg-teal-200 text-teal-800 text-xs px-2 inline-block rounded-full uppercase font-semibold tracking-wide">
                             New
                           </span>
                           <div className="ml-2 text-gray-600 uppercase text-xs font-semibold tracking-wider">
-                            2 baths &bull; 3 rooms
+                          &bull; Product Of TechTrove
                           </div>
                         </div>
                         <h4 className="mt-2 text-sm font-semibold uppercase leading-tight truncate">
                           {product.productName}
                         </h4>
                         <div className="mt-2 text-xl">
-                        ৳ {product.price}
+                          ৳ {product.price}
                           <span className="text-gray-600 text-base"> /BD</span>
                         </div>
                         <div className="mt-1">
@@ -76,7 +107,13 @@ const TechTroveProducts = () => {
                             {product.productColor}/color{" "}
                           </span>
                           <span className="text-base text-gray-600">
-                            <Link className="hover:underline"> (View All Details)</Link>
+                            <Link
+                              to="#"
+                              className="hover:underline"
+                              onClick={() => openModal(product)}
+                            >
+                              (View All Details)
+                            </Link>
                           </span>
                         </div>
                       </div>
@@ -93,4 +130,3 @@ const TechTroveProducts = () => {
 };
 
 export default TechTroveProducts;
-
