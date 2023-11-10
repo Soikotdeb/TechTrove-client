5d5afc90c90d6fb6267f497121a687d996df5635
soikotdeb <sdsoikot424@gmail.com>
2023-11-10 12:27:36 +0600

    usecart add every 3 second refetch

--- src/Hook/useCart.jsx
+++ src/Hook/useCart.jsx
@@ -1,31 +1,33 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { AuthContext } from "../Provider/AuthProvider";
 import useAxiosSecure from "./useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
 
-
 const useCart = () => {
-const {user,loading}= useContext(AuthContext)
+  const { user, loading } = useContext(AuthContext);
 
-// const token = localStorage.getItem('access-token')
-const [axiosSecure] = useAxiosSecure();
+  const [axiosSecure] = useAxiosSecure();
 
-const { refetch, data: cart = [] } = useQuery({
+  const { refetch, data: cart = [] } = useQuery({
     queryKey: ['carts', user?.email],
-    enabled:!loading,
-    // queryFn: async () => {
-    //     const res = await fetch(`https://bistro-boss-server-orcin.vercel.app/carts?email=${user?.email}`,{headers:{
-    //         authorization: `bearer ${token}`
-    //     }})
-    //     return res.json();
-    // },
+    enabled: !loading,
     queryFn: async () => {
-        const res = await axiosSecure(`/carts?email=${user?.email}`)
-        return res.data;
+      const res = await axiosSecure(`/carts?email=${user?.email}`);
+      return res.data;
     },
-})
+  });
 
-return [cart, refetch]
+  useEffect(() => {
+    // Set up interval for refetching every 2 seconds
+    const intervalId = setInterval(() => {
+      refetch();
+    }, 2000);
 
-}
+    // Clean up the interval when the component unmounts
+    return () => clearInterval(intervalId);
+  }, [refetch]);
+
+  return [cart, refetch];
+};
+
 export default useCart;
