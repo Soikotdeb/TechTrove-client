76a125667cb593d97d1b1a4bdbc789e0d40c971e
soikotdeb <sdsoikot424@gmail.com>
2023-12-07 17:48:16 +0600

    checkout page design changed

--- src/Pages/ProductCart/CheckoutPay.jsx
+++ src/Pages/ProductCart/CheckoutPay.jsx
@@ -5,64 +5,76 @@ import { loadStripe } from '@stripe/stripe-js';
 import { FaArrowLeft } from 'react-icons/fa';
 import { CardElement } from '@stripe/react-stripe-js';
 
-// Retrieve your Stripe public key from the environment variable
 const stripePublicKey = import.meta.env.VITE_PAYMENT;
-
-// Make sure the stripePublicKey is properly set in your environment
 const stripePromise = loadStripe(stripePublicKey);
-
-
 
 const CheckoutPay = () => {
   const handleSubmit = async (event) => {
-    event.preventDefault()
+    event.preventDefault();
     // Handle Stripe payment submission logic here
   };
+  
   const selectedProduct = localStorage.getItem('SelectedProduct');
   const parsedSelectedProduct = JSON.parse(selectedProduct);
-  
   const TotalAmount = localStorage.getItem('TotalAmount');
+
   return (
-    <div>
-      <div className="flex flex-row">
-        <Link to="/productCart" className="flex items-center gap-1 hover:underline">
+    <div className="p-4">
+      <div className="flex items-center mb-4">
+        <Link to="/productCart" className="flex items-center gap-1 text-blue-500 hover:underline">
           <FaArrowLeft /> Continue Payment
         </Link>
       </div>
-      <div className='grid lg:grid-cols-8 md:grid-cols-5 sm:grid-cols-3 gap-4 p-2'>
-  {parsedSelectedProduct.map(product => (
-    <div key={product._id} className='grid-item'>
-      <img className='w-full h-auto' src={product.image} alt={product.name} />
-    </div>
-  ))}
-</div>
 
-
-      <div>
-        <p className='mb-5'><small>Total Amount: {TotalAmount}</small></p>
-        <Elements stripe={stripePromise}>
-          <form onSubmit={handleSubmit}>
-            <CardElement
-              options={{
-                style: {
-                  base: {
-                    fontSize: '16px',
-                    color: '#424770',
-                    '::placeholder': {
-                      color: '#aab7c4',
+      <div className="mt-6">
+        <p className='mb-3 text-lg'><small>Total Amount: ৳{TotalAmount}.00</small></p>
+        <div className="w-96 mx-auto border rounded p-2">
+          <Elements stripe={stripePromise}>
+            <form onSubmit={handleSubmit}>
+              <CardElement
+                options={{
+                  style: {
+                    base: {
+                      fontSize: '16px',
+                      color: '#424770',
+                      '::placeholder': {
+                        color: '#aab7c4',
+                      },
+                    },
+                    invalid: {
+                      color: '#9e2146',
                     },
                   },
-                  invalid: {
-                    color: '#9e2146',
-                  },
-                },
-              }}
+                }}
+              />
+              <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mt-4 w-full" type="submit">
+                Pay Now!
+              </button>
+            </form>
+          </Elements>
+        </div>
+        <div className="mt-4 flex justify-center items-center">
+          <div className="flex justify-between w-44">
+            <img
+              src="https://i.ibb.co/vxgjPv5/bkash1.png" 
+              alt="Bkash"
+              className="w-12 h-12 rounded-md transition duration-300 transform hover:rounded-full hover:scale-110 cursor-pointer"
             />
-            <button className="btn btn-outline btn-accent mt-6 align-middle flex mx-auto" type="submit">
-              Pay Now!
-            </button>
-          </form>
-        </Elements>
+            <img
+              src="https://i.ibb.co/YLML4Vh/nagad.jpg"
+              alt="Nagad"
+              className="w-12 h-12 rounded-md transition duration-300 transform hover:rounded-full hover:scale-110 cursor-pointer"
+            />
+            <img
+              src="https://i.ibb.co/j31xJmY/roket.jpg" 
+              alt="Roket"
+              className="w-12 h-12 rounded-md transition duration-300 transform hover:rounded-full hover:scale-110 cursor-pointer"
+            />
+          </div>
+        </div>
+        <div className="mt-4 text-center text-gray-500">
+          <p>Bkash, Nagad, Roket payment systems are coming soon!</p>
+        </div>
       </div>
     </div>
   );
